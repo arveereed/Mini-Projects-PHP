1c66d206ad40423a393784c1970273cef5a16df8
Arvee <arveedurante220@gmail.com>
2024-04-08 20:09:33 +0800

    I separate the add book component

--- CRUD/index.php
+++ CRUD/index.php
@@ -8,36 +8,7 @@
   </head>
   <body>
     <div class="container">
-      <header class='d-flex justify-content-between my-4'>
-        <h1>Add new Book</h1>
-        <div>
-          <a href="#" class='btn btn-primary'>Back</a>
-        </div>
-      </header>
-
-      <form action="inc/add_book.inc.php" method="post">
-        <div class="form-element my-4">
-          <input type="text" class="form-control" name='title' placeholder="Book Title: ">
-        </div>
-        <div class="form-element my-4">
-          <input type="text" class="form-control" name='author' placeholder="Author Name: ">
-        </div>
-        <div class="form-element my-4">
-          <select name="type" class="form-control">
-            <option value="">Select Book Type</option>
-            <option value="adventure">Adventure</option>
-            <option value="fantasy">Fantasy</option>
-            <option value="scifi">Sci-Fi</option>
-            <option value="horror">Horror</option>
-          </select>
-        </div>
-        <div class="form-element my-4">
-          <input type="text" class="form-control" name="description" placeholder='Book Description:'>
-        </div>
-        <div class="form-element">
-          <button class="btn btn-success">Add Book</button>
-        </div>
-      </form>
+      
     </div>
   </body>
 </html>
